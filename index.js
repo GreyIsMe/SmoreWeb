b4d72af3a969a849664f16d83269aace7cbae111
Chronomly <chronomly@gmail.com>
2017-10-01 18:48:38 -0400

    Initial commit #2

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+function running() {
+    console.log(`listening on port: 80`)
+};
+
+app.use(express.static("public_html"));
+
+app.get('/discord', function(req, res){
+   res.redirect(301, `https://discord.io/chrono`)
+   console.log(`Discord Redirect sent to ${req.ip}`)
+   next()
+});
+
+app.get('/*', function(req, res){
+   res.sendFile(__dirname + '/public_html/error.html');
+   console.log(`Error Page sent to ${req.ip}`)
+});
+ 
+app.listen(80, running);
