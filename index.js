622581e0654e749ddcd04f3b091da98009388c10
Chronomly <chronomly@gmail.com>
2017-10-03 21:38:27 -0400

    fix things

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+app.use(helmet())
 
 function running() {
-    console.log(`listening on port: 80`)
+    console.log(`listening on port: 80`);
 };
 
 app.use(express.static("public_html"));
 
 app.get('/discord', function(req, res){
-   res.redirect(301, `https://discord.gg/6P6MNAU`)
-   console.log(`Discord Redirect sent to ${req.ip}`)
+   res.redirect(301, `https://discord.gg/6P6MNAU`);
+   console.log(`Discord Redirect sent to ${req.ip}`);
 });
 
 app.get('/contact', function(req, res){
-    res.redirect(301, `mailto:smorebtofficail@gmail.com`)
-    console.log(`Email Redirect sent to ${req.ip}`)
- });
+    res.redirect(301, `mailto:smorebtofficail@gmail.com`);
+    console.log(`Email Redirect sent to ${req.ip}`);
+});
 
 app.get('/bots/:bot', function(req, res){
     if(req.params.bot === 'smore') {
-        res.redirect(301, `https://discordapp.com/oauth2/authorize?permissions=360054015&scope=bot&client_id=290228059599142913`)
-        console.log(`Smore Redirect sent to ${req.ip}`)
+        res.redirect(301, `https://discordapp.com/oauth2/authorize?permissions=360054015&scope=bot&client_id=290228059599142913`);
+        console.log(`Smore Redirect sent to ${req.ip}`);
     } else {
-        res.send('Please provide a valid bot')
-    }
+        res.send('Please provide a valid bot');
+    };
 });
 
 app.get('/*', function(req, res){
    res.sendFile(__dirname + '/public_html/404.html');
-   console.log(`Error Page sent to ${req.ip}`)
+   console.log(`Error Page sent to ${req.ip}`);
 });
  
 app.listen(80, running);
